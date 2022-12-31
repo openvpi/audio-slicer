cd1ab55c093e5e180b93e63044e1af3bebbb9cf0
Sine Striker <trueful@163.com>
2023-01-01 00:41:32 +0800

    Fix pyqt problem

--- slicer-gui.py
+++ slicer-gui.py
@@ -4,17 +4,18 @@ from PySide6 import QtCore
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QFont
 
-import mainwindow
+import gui.mainwindow
 
 if __name__ == '__main__':
-    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(QtCore.Qt.ApplicationAttribute.AA_EnableHighDpiScaling)
+    
     app = QApplication(sys.argv)
 
     font = QFont('Microsoft YaHei UI')
     font.setPixelSize(12)
     app.setFont(font)
 
-    window = mainwindow.MainWindow()
+    window = gui.mainwindow.MainWindow()
     window.show()
 
     sys.exit(app.exec())
